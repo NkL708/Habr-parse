9d3f3b14b39d65add1ce1fe68d36e2f03e6dbd92
NkL708 <nklnsk708@gmail.com>
2022-09-27 17:58:47 +0700

    Beautified code

--- main.py
+++ main.py
@@ -1,81 +1,135 @@
-from distutils.text_file import TextFile
-from email.mime.text import MIMEText
+import os
 import requests
 import codecs
-import smtplib
-from email.message import EmailMessage
 
-from cmath import cos
 from bs4 import BeautifulSoup
 
 
-def parse():
-    url = 'https://freelance.habr.com/tasks?categories=development_all_inclusive%2Cdevelopment_backend%2Cdevelopment_frontend%2Cdevelopment_bots%2Cdevelopment_scripts%2Cdevelopment_other'
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-    }
-    response = requests.get(url, headers=header)
+file_name = 'data.txt'
+site_url = 'https://freelance.habr.com/'
+get_tasks_url = 'https://freelance.habr.com/tasks?categories=development_all_inclusive%2Cdevelopment_backend%2Cdevelopment_frontend%2Cdevelopment_bots%2Cdevelopment_scripts%2Cdevelopment_other'
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+}
 
-    if response.status_code == 200:
-        with codecs.open('data.txt', 'w', 'utf-16') as f:
-            page = BeautifulSoup(response.text, 'html.parser')
-            list = page.find('ul', attrs={'class': 'content-list'})
-            list_items = list.findAll('li', attrs={'class': 'content-list__item'})
-            for list_item in list_items:
-                site_url = 'https://freelance.habr.com/'  
-                
-                task_title_div = list_item.find('div', attrs={'class': 'task__title'})
-                
-                task_text = task_title_div['title']
-                print(task_text, file=f)
-                
-                task_link = task_title_div.find('a')['href']
-                task_link = site_url + task_link
-                print(task_link, file=f)
-                
-                # Parse description
-                desc_response = requests.get(task_link, headers=header)
-                if desc_response.status_code == 200:
-                    page = BeautifulSoup(desc_response.text, 'html.parser')
-                    desc = page.find('div', attrs={'class': 'task__description'})
-                    # Replacing break lines
-                    for br in desc.find_all('br'):
-                        br.replace_with('\n')
-                    print(desc.text, file=f)
-                
-                views = list_item.find('span', attrs={'class': 'params__views'}).find('i').text
-                print(f'{views} просмотра', file=f)
-                
-                response = list_item.find('span', attrs={'class': 'params__responses'})
-                if response:
-                    response = response.find('i').text
-                    print(f'{response} отклика', file=f)
-                
-                time_ago = list_item.find('span', attrs={'class': 'params__published-at'}).find('span').text
-                print(f'{time_ago}', file=f)
-                
-                cost = list_item.find('span', attrs={'class': 'count'})
-                if cost:
-                    cost = cost.text
-                    print(cost, file=f)
-                else:
-                    print('Договорная', file=f)
-                
-                separator = ''
-                for i in range(100):
-                    separator += '='
-                print(file=f)
-                print(separator, file=f)
-                print(file=f)
-            # with open(f, 'rb') as fp:
-            #     msg = EmailMessage()
-            #     msg.set_content(fp.read())
-            # msg['Subject'] = f'The contents of {f}'
-            # msg['From'] = 'kek'
-            # msg['To'] = 'nklnsk708@gmail.com'
-            # s = smtplib.SMTP('localhost')
-            # s.send_message(msg)
-            # s.quit()
 
-parse()
+def get_response(url):
+    tasks_response = requests.get(url, headers=header)
+    return tasks_response
+
+
+def get_list_items(tasks_response):
+    page = BeautifulSoup(tasks_response.text, 'html.parser')
+    list = page.find('ul', attrs={'class': 'content-list'})
+    list_items = list.findAll('li', attrs={'class': 'content-list__item'})
+    return list_items
+
+
+def get_task_title_div(list_item):
+    task_title_div = list_item.find('div', attrs={'class': 'task__title'})
+    return task_title_div
+
+
+def print_to_file(text, file_name):
+    with codecs.open(file_name, 'a', 'utf-8') as f:
+        print(text, file=f)
+
+
+def print_task_text(list_item):
+    task_title_div = get_task_title_div(list_item)
+    task_text = task_title_div['title']
+    print_to_file(task_text, file_name)
+
+
+def print_task_link(list_item):
+    task_url = site_url + get_task_url(list_item)
+    print_to_file(task_url, file_name)
+
+
+def get_task_url(list_item):
+    task_title_div = get_task_title_div(list_item)
+    task_url = task_title_div.find('a')['href']
+    return task_url
+
+
+def print_description(list_item):
+    task_url = get_task_url(list_item)
+    url = site_url + task_url
+    desc_response = get_response(url)
+    if desc_response.status_code != 200:
+        return
+    page = BeautifulSoup(desc_response.text, 'html.parser')
+    desc = page.find('div', attrs={'class': 'task__description'})
+    # Replacing break lines
+    for br in desc.find_all('br'):
+        br.replace_with('\n')
+    print_to_file(desc.text, file_name)
+
+
+def print_views(list_item):
+    views = list_item.find(
+        'span', attrs={'class': 'params__views'}).find('i').text
+    views = f'{views} просмотра'
+    print_to_file(views, file_name)
+
+
+def print_tasks_response(list_item):
+    tasks_response = list_item.find(
+        'span', attrs={'class': 'params__responses'})
+    if tasks_response:
+        tasks_response = tasks_response.find('i').text
+        tasks_response = f'{tasks_response} отклика'
+        print_to_file(tasks_response, file_name)
+
+
+def print_time_ago(list_item):
+    time_ago = list_item.find(
+        'span', attrs={'class': 'params__published-at'}).find('span').text
+    print_to_file(time_ago, file_name)
+
+
+def print_cost(list_item):
+    cost = list_item.find('span', attrs={'class': 'count'})
+    if cost:
+        cost = cost.text
+        print_to_file(cost, file_name)
+    else:
+        print_to_file('Договорная', file_name)
+
+
+def print_separator():
+    separator = ''
+    for i in range(100):
+        separator += '='
+    print_to_file('', file_name)
+    print_to_file(separator, file_name)
+    print_to_file('', file_name)
+
+
+def get_task_column(list_item):
+    task_column = list_item.find('div', attrs={'class': 'task__column_desc'})
+    return task_column
+
+
+def parse_tasks():
+    try:
+        os.remove(file_name)
+    except OSError:
+        pass
+    tasks_response = get_response(get_tasks_url)
+    if tasks_response.status_code != 200:
+        return
+    list_items = get_list_items(tasks_response)
+    for list_item in list_items:
+        print_task_text(list_item)
+        print_task_link(list_item)
+        print_description(list_item)
+        print_description(list_item)
+        print_views(list_item)
+        print_time_ago(list_item)
+        print_cost(list_item)
+        print_separator()
+
+
+parse_tasks()
